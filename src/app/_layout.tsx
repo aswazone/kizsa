d1550367c1407942e9b7c2bad0bf149d1d0b7172
aswazone <itsme.aswinkp@gmail.com>
2026-03-06 22:53:58 +0530

    Error: Element type is invalid !!

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,32 +3,40 @@ import { tokenCache } from "@clerk/expo/token-cache";
 import { Stack } from "expo-router";
 import "../../global.css";
 
+// import * as Sentry from '@sentry/react-native';
+
+// Sentry.init({
+//   dsn: 'https://c4f4cf5be5155be2ca6f61ae26827c3b@o4510973455761408.ingest.us.sentry.io/4510998304522240',
+
+//   // Adds more context data to events (IP address, cookies, user, etc.)
+//   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+//   sendDefaultPii: true,
+
+//   // Enable Logs
+//   enableLogs: true,
+
+//   // Configure Session Replay
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1,
+//   integrations: [Sentry.mobileReplayIntegration()],
+
+//   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+//   // spotlight: __DEV__,
+// });
+
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
   throw new Error("Missing Publishable Key");
 }
 
-function RootStack() {
-  const { isSignedIn } = useAuth();
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={!isSignedIn}>
-        <Stack.Screen name="(auth)" />
-      </Stack.Protected>
-
-      <Stack.Protected guard={!!isSignedIn}>
-        <Stack.Screen name="(tabs)" />
-      </Stack.Protected>
-    </Stack>
-  );
-}
-
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <RootStack />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
     </ClerkProvider>
   );
-}
+};
